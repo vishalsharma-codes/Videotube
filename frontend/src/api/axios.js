834ab1d43064b98bc5b7b9c2b8d18ef91eee0d21
Your Name <youremail@example.comV>
2026-07-30 23:27:53 +0530

    Update axios base URL to include API version

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"https://videotube-u9v7.onrender.com",
+    baseURL:"https://videotube-u9v7.onrender.com/api/v1",
     withCredentials:true
 })
